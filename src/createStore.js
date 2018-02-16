8999c36c053f542e9241d96d0a6d868222c55fd2
Marvin Clerge <clergemarvin@gmail.com>
2018-02-16 11:35:41 -0500

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,5 +1,22 @@
 export default function createStore(reducer) {
-  // add your code here
+  let state;
+
+  function getState() {
+    return state
+  }
+  
+  function dispatch(action) {
+    state = reducer(state, action)
+    render()
+  }
+  
+  dispatch({type: "@@REDUX/INIT"})
+  
+  return {
+    getState: getState,
+    dispatch: dispatch
+  }
+  
 }
 
 function render() {
