57d17d20ab80888d08eac1a737ef0c3e5a08c2dc
humzahchoudry <Humzah.choudry@gmail.com>
2018-02-18 09:52:43 -0500

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,5 +1,21 @@
 export default function createStore(reducer) {
-  // add your code here
+  let state;
+
+  function dispatch(action) {
+    state = reducer(state, action)
+    render()
+  }
+
+  function getState() {
+    return state
+  }
+
+  dispatch({type: 'initial'})
+
+  return {
+    dispatch: dispatch,
+    getState: getState
+  }
 }
 
 function render() {
